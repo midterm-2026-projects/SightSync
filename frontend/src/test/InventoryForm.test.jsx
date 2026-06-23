bb6f5157f686baffb9e519a31c03d820bb08db17
Manalo-2A <dextermanalo2508@gmail.com>
2026-06-23 12:57:01 +0800

    Create Inventory Management Interface • InventoryPage.jsx • InventoryTable.jsx • InventoryForm.jsx

--- frontend/src/test/InventoryForm.test.jsx
+++ frontend/src/test/InventoryForm.test.jsx
@@ -1,4 +1,5 @@
 import "@testing-library/jest-dom";
+import { describe, test, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import InventoryForm from "../components/InventoryForm";
 
