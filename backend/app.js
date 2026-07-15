1b3c4aeeea4b63770b39d41b73f9471a3018f8ac
John-Patrick-Russel-Lalo <johnpatricklalo@gmail.com>
2026-07-15 15:48:27 +0800

    week 4 day 1

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ import express from "express";
 import inventoryRoutes from "./src/objective2/routes/inventory.js";
 import patientRoutes from "./src/objective1/routes/patient.routes.js";
 import appointmentRoutes from "./src/objective1/routes/appointment.routes.js";
-import doctorAvailabilityRoutes from "./src/objective1/routes/doctorAvailability.routes.js";
+// import doctorAvailabilityRoutes from "./src/objective1/routes/doctorAvailability.routes.js";
 
 const app = express();
 
@@ -18,6 +18,6 @@ app.get("/", (req, res) => {
 app.use("/inventory", inventoryRoutes);
 app.use("/api/patients", patientRoutes);
 app.use("/api/appointments", appointmentRoutes);
-app.use("/api/doc-availability", doctorAvailabilityRoutes);
+// app.use("/api/doc-availability", doctorAvailabilityRoutes);
 
 export default app;
