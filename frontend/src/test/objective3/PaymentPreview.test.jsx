1b702311350eee496bec43f82e734b7e013a66b6
hazelannmanimtim02 <23-12141@g.batstate-u.edu.ph>
2026-07-15 10:43:03 +0800

    Design Notification System

--- frontend/src/test/objective3/PaymentPreview.test.jsx
+++ frontend/src/test/objective3/PaymentPreview.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
-import ReceiptPreview from "../../components/Objective3/ReceiptPreview";
+import ReceiptPreview from "../../components/objective3/ReceiptPreview";
 
 describe("Receipt Preview", () => {
 
